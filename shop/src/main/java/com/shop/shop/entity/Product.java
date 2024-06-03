023fa2b7b27feadba23da875641b762771fbc14e
bartoszkocylo <bartoszk236@gmail.com>
2024-06-03 22:58:00 +0200

    First entity "Product" and tests.

---
+++ shop/src/main/java/com/shop/shop/entity/Product.java
@@ -0,0 +1,32 @@
+package com.shop.shop.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private String category;
+}
---
+++ shop/src/test/java/com/shop/shop/ShopApplicationTests.java
@@ -0,0 +1,67 @@
+package com.shop.shop;
+
+import com.shop.shop.entity.Product;
+import com.shop.shop.repository.ProductRepository;
+import com.shop.shop.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Autowired
+	private ProductService productService;
+
+	@Test
+	void addProduct() {
+		Product product = new Product();
+		product.setName("Szklanka");
+		product.setDescription("Szklanka o pojemności 250ml. Kolor przezroczysty");
+		product.setPrice(new BigDecimal("5.99"));
+		product.setCategory("Zastawa");
+		productService.save(product);
+	}
+
+	@Test
+	void getAllProduct(){
+		List<Product> products = productService.findAll();
+
+		products.forEach(product -> {
+			System.out.println(product.getName());
+			System.out.println(product.getDescription());
+			System.out.println(product.getPrice());
+			System.out.println(product.getCategory());
+		});
+	}
+
+	@Test
+	void deleteProduct(){
+		productService.deleteById(3L);
+	}
+
+	@Test
+	void getById(){
+		Optional<Product> productOptional = productService.findById(4L);
+
+		if(productOptional.isPresent()) {
+			Product product = productOptional.get();
+			System.out.println(product.getName());
+			System.out.println(product.getDescription());
+			System.out.println(product.getPrice());
+			System.out.println(product.getCategory());
+		} else {
+			System.out.println("Produkt nie istnieje");
+		}
+	}
+
+}
